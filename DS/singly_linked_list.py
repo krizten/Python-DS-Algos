9ecc6ec4d01ff9f3e63aff08ca384b0af50eb62f
Christian Effiong <norexus007@gmail.com>
2020-01-02 19:01:15 +0100

    test traverse_list method

--- DS/singly_linked_list.py
+++ DS/singly_linked_list.py
@@ -13,7 +13,7 @@ class SinglyLinkedList:
 
     def traverse_list(self):
         if self.head_node is None:
-            return None
+            yield None
         else:
             value = self.head_node
             while value is not None:
@@ -24,16 +24,17 @@ class SinglyLinkedList:
 
 # ======== TEST ===========
 list_1 = SinglyLinkedList()
-list_1.head = Node(13)
+list_1.head_node = Node(13)
 node_2 = Node(21)
 node_3 = Node(34)
 node_4 = Node(55)
 node_5 = Node(89)
 
-list_1.head.next_node = node_2
+list_1.head_node.next_node = node_2
 node_2.next_node = node_3
 node_3.next_node = node_4
 node_4.next_node = node_5
 
-print(list_1.traverse_list())
+for item in list_1.traverse_list():
+    print(item)
 # ======== TEST ===========
